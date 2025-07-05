ec39c8b06d5b8b4dc3c311feaba4861128a1e558
implicitfield <114500360+implicitfield@users.noreply.github.com>
2025-07-06 02:38:46 +0300

    Connect to auxiliary servers in a one-by-one fashion
    It might be the case that trying to do this in parallel destroys the NAT
    mappings.

--- wait.sh
+++ wait.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
+set -euxo pipefail
 
 function fetch_json {
   JSON=$(curl -L \
@@ -8,6 +8,10 @@ function fetch_json {
     -H "Authorization: Bearer $GITHUB_TOKEN" \
     -H "X-GitHub-Api-Version: 2022-11-28" \
     "https://api.github.com/repos/$FULL_REPOSITORY_NAME/actions/runs/$RUN_ID/artifacts")
+}
+
+function recheck_connections {
+  CONNECTIONS="$(sudo wg show | grep 'latest handshake' | wc -l | tr -d ' ')" || true
 }
 
 # Wait for auxiliary runners to come online (if they haven't already done so).
@@ -22,6 +26,8 @@ done
 for i in $(seq 0 $(($1 - 1))); do
   echo "${JSON}" | jq -c '.artifacts.[]' | while read entry; do
     if [[ $(echo "${entry}" | jq ".name" | sed 's/"//g') == "Auxiliary${i}IP" ]]; then
+      recheck_connections
+      OLD_CONNECTIONS=$CONNECTIONS
       ARTIFACT_ID=$(echo "${entry}" | jq ".id")
       curl -L \
         -H "Accept: application/vnd.github+json" \
@@ -35,12 +41,12 @@ for i in $(seq 0 $(($1 - 1))); do
       # Perform the hole punch.
       # TTL is set to 4 to avoid actually delivering the packet all the way (while still being high enough to hole punch).
       sudo nping --udp --ttl 4 --no-capture --source-port 1024 --count 60 --delay 10s --dest-port $PORT $IP &
+      until [[ $(($OLD_CONNECTIONS + 1)) -eq $CONNECTIONS ]]; do
+        sudo wg show
+        sleep 1
+        recheck_connections
+      done
+      sudo killall nping
     fi
   done
-done
-
-# Wait for the peers to connect.
-until [[ $(sudo wg show | grep 'latest handshake' | wc -l) -eq $1 ]]; do
-  sudo wg show
-  sleep 1
 done

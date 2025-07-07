13ac4b161c4459905ee43c1e9768b3123399ca73
implicitfield <114500360+implicitfield@users.noreply.github.com>
2025-07-07 03:47:27 +0300

    Generate DISTCC_HOSTS dynamically

--- build.sh
+++ build.sh
@@ -2,8 +2,11 @@
 
 set -euo pipefail
 
+export DISTCC_HOSTS="localhost/4"
+for i in $(seq 1 $1); do
+  export DISTCC_HOSTS="$DISTCC_HOSTS 192.168.166.$(($i + 1))/5"
+done
 export TARGET="$(clang -v 2>&1 | grep 'Target:' | cut -d ' ' -f2)"
-export DISTCC_HOSTS="localhost/4 192.168.166.2/5 192.168.166.3/5"
 export SDKROOT="$(xcrun --show-sdk-path)"
 cd llvm-project
 patch -p1 < ../clang-actually-disable-pedantic.patch
@@ -20,7 +23,9 @@ cmake -G Ninja \
         -DLLVM_ENABLE_RUNTIMES="compiler-rt;libcxx;libcxxabi;libunwind" \
         -DLLVM_ENABLE_PEDANTIC=OFF \
         llvm
-ninja -j28
+JOBS=$(((4 + ($1 * 5)) * 2))
+echo "Building with $JOBS jobs"
+ninja -j$JOBS
 mkdir LLVM-20.1.7
 DESTDIR=$PWD/LLVM-20.1.7 ninja install
 tar -cf - LLVM-20.1.7 | xz -9 -T$(sysctl -n hw.logicalcpu) > ../LLVM-20.1.7.tar.xz

18364ae4597a935e395c5538071352ed36db49e5
implicitfield <114500360+implicitfield@users.noreply.github.com>
2025-09-07 02:19:45 +0300

    Delay connection attempts

--- sync_pause.sh
+++ sync_pause.sh
@@ -10,3 +10,17 @@ until [[ $(echo "${JSON}" | jq '.total_count') -eq $TARGET ]]; do
   sleep 2
   fetch_json
 done
+
+# Generate peer entries.
+for i in $(seq 0 $(($1 - 1))); do
+  ARTIFACT_ID=$(get_artifact_id "Auxiliary${i}Data")
+  fetch_artifact "$ARTIFACT_ID" "auxdata.zip"
+  unzip auxdata.zip
+  export PUBLIC_KEY="$(openssl enc -d -aes-256-cbc -pbkdf2 -iter 20000 -in Auxiliary${i}Data.txt -pass env:ENCRYPTION_KEY | cut -d ':' -f3)"
+  cat << EOF >> wg0-primary.conf
+[Peer]
+PublicKey = $PUBLIC_KEY
+AllowedIPs = 192.168.166.$(($i + 2))/32
+PersistentKeepalive = 25
+EOF
+done
--- wait.sh
+++ wait.sh
@@ -3,24 +3,8 @@
 set -euo pipefail
 source common.sh
 
-fetch_json
-
 sudo mkdir -p /etc/wireguard
 sudo rm -f /etc/wireguard/wg0.conf
-
-# Generate peer entries.
-for i in $(seq 0 $(($1 - 1))); do
-  ARTIFACT_ID=$(get_artifact_id "Auxiliary${i}Data")
-  fetch_artifact "$ARTIFACT_ID" "auxdata.zip"
-  unzip auxdata.zip
-  export PUBLIC_KEY="$(openssl enc -d -aes-256-cbc -pbkdf2 -iter 20000 -in Auxiliary${i}Data.txt -pass env:ENCRYPTION_KEY | cut -d ':' -f3)"
-  cat << EOF >> wg0-primary.conf
-[Peer]
-PublicKey = $PUBLIC_KEY
-AllowedIPs = 192.168.166.$(($i + 2))/32
-PersistentKeepalive = 25
-EOF
-done
 
 sudo cp wg0-primary.conf /etc/wireguard/wg0.conf
 rm wg0-primary.conf
